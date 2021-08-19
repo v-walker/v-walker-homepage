fb2c34c35372a3e1f43974e2c5eebe2043670f0f
Victoria Walker <vmsmith2014@gmail.com>
2021-08-19 13:30:24 -0400

    update errors and troubleshooting1#

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,7 @@ let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
 function setUserName() {
-    let myName = prompt('Please enter your name.');
+    let myName = prompt('Please enter your name for a personalized experience!');
     localStorage.setItem('name', myName);
     myHeading.textContent = 'Welcome, ' + myName;
 }
